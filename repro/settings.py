69b47b9af23711b8fc68d513e2bfde94b257d56d
Wei Liang <weilaingtuina@gmail.com>
2021-02-24 11:23:42 +0800

    a

--- repro/settings.py
+++ repro/settings.py
@@ -1,6 +1,6 @@
+import django_heroku
 from pathlib import Path
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,17 +112,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
+# Activate Django-Heroku.
 django_heroku.settings(locals())
-

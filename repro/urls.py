edd019c8e7acc61a8254bcc1a2bb7baa8487ea2f
Wei Liang <weilaingtuina@gmail.com>
2021-02-24 16:25:10 +0800

    afregw

--- repro/urls.py
+++ repro/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reapp.urls')),
-    url('static/', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
